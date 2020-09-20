c347fd16078594f584f48837461f7f41c2a52e78
Fabian Fichter <ianfab@users.noreply.github.com>
2020-09-20 19:57:51 +0200

    Disable futility pruning for captures

--- src/search.cpp
+++ src/search.cpp
@@ -1129,6 +1129,7 @@ moves_loop: // When in check, search starts from here
                   && !(PvNode && abs(bestValue) < 2)
                   && PieceValue[MG][type_of(movedPiece)] >= PieceValue[MG][type_of(pos.piece_on(to_sq(move)))]
                   && !ss->inCheck
+                  && !pos.captures_to_hand()
                   && ss->staticEval + 267 + 391 * lmrDepth
                      + PieceValue[MG][type_of(pos.piece_on(to_sq(move)))] <= alpha)
                   continue;
